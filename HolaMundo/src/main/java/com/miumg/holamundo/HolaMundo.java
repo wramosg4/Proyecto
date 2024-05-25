7744a76707a0dbbb6fe1004b7a8b3e904648797a
wramosg4 <wramosg4@miumg.edu.gt>
2024-05-25 17:32:01 -0600

    Proyecto Inicial

---
+++ HolaMundo/src/main/java/com/miumg/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miumg.holamundo;
+
+/**
+ *
+ * @author ramos
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
